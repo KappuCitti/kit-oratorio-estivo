f2c438c8a474682000689a918f84c94d1e6067e8
Benjo332 <manuelparma2005@gmail.com>
2025-03-18 23:09:33 +0100

    SERVER - Added json logs output to file

--- src/server/TODO.md
+++ src/server/TODO.md
@@ -1,9 +1,9 @@
 # Things I will probably forgor to do
 
-- [ ] Create a better error handling system for the database
+- [x] Create a better error handling system for the database
 - [x] Add a custom error for body validation
 - [ ] Implements all settings both in the config file and the env file
 - [x] Add an option to choose http/https
 - [x] Add an option to choose a domain for cors
-- [ ] Make logger write json logs to file
-- [ ] Put boundaries and limits to the body parameters
+- [x] Make logger write json logs to file
+- [x] Put boundaries and limits to the body parameters
--- src/server/src/utils/logger.ts
+++ src/server/src/utils/logger.ts
@@ -1,17 +1,32 @@
 import config from '@/config';
 import pino from 'pino';
-import pretty from 'pino-pretty';
+import path from 'path';
 
 export function createLogger(name?: string, format?: string) {
-  return pino(
-    {
-      level: config.logLevel,
-      name,
+  return pino({
+    level: config.logLevel,
+    name,
+    transport: {
+      pipeline: [
+        {
+          target: 'pino-pretty',
+          options: {
+            colorize: true,
+            messageFormat: format,
+            hideObject: true,
+          },
+        },
+        {
+          target: 'pino-roll',
+          options: {
+            file: path.join(config.logFolder, 'log'),
+            extension: '.log',
+            frequency: 'daily',
+            dateFormat: 'yyyy-MM-dd',
+            mkdir: true,
+          },
+        },
+      ],
     },
-    pretty({
-      colorize: true,
-      messageFormat: format,
-      hideObject: true,
-    })
-  );
+  });
 }
