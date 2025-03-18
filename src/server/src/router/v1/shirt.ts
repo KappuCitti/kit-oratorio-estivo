122ffb2c3b5ff1d359e6487e0b18907b5dd5b03c
Benjo332 <manuelparma2005@gmail.com>
2025-03-18 23:57:41 +0100

    SERVER - Added DEL /shirt/id size to router

--- src/server/src/router/v1/shirt.ts
+++ src/server/src/router/v1/shirt.ts
@@ -1,10 +1,13 @@
 import createShirtController from '@/controllers/shirt/createShirt';
+import deleteShirtController from '@/controllers/shirt/deleteShirt';
 import getShirtsController from '@/controllers/shirt/getShirtList';
 import type { HonoApp } from '@/models/app.model';
 import { createShirtRouteDef } from '@/openapi/shirt/createShirt';
+import { deleteShirtRouteDef } from '@/openapi/shirt/deleteShirt';
 import { getShirtListRouteDef } from '@/openapi/shirt/getShirts';
 
 export default (router: HonoApp) => {
   router.openapi(getShirtListRouteDef, getShirtsController);
+  router.openapi(deleteShirtRouteDef, deleteShirtController);
   router.openapi(createShirtRouteDef, createShirtController);
 };
