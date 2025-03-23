2a38f87776b0c0c798067317ce0eb0b2c05f4607
Benjo332 <manuelparma2005@gmail.com>
2025-03-23 22:09:18 +0100

    SERVER - Fixed crash when trying to use DEL /parent/id route

--- src/server/src/database/parent/deleteParent.ts
+++ src/server/src/database/parent/deleteParent.ts
@@ -3,22 +3,30 @@ import { dbLogger } from '../logger';
 import { HttpStatusCodes } from '@/codes';
 import { db } from '..';
 import { childParentTable, childTable, parentTable } from '../schema';
-import { eq, inArray } from 'drizzle-orm';
+import { count, eq, inArray, sql } from 'drizzle-orm';
 
-export async function deleteParent(id: number, deleteChildren: boolean) {
+export async function deleteParent(id: number, deleteChildrens: boolean) {
   try {
-    const [{ affectedRows }] = await db
-      .delete(parentTable)
-      .where(eq(parentTable.id, id));
-    if (affectedRows > 0) {
-      const subQuery = db
-        .select()
-        .from(childParentTable)
-        .where(eq(childParentTable.parentId, id))
-        .as('childParentTable');
-      await db
-        .delete(childTable)
-        .where(inArray(childTable.id, subQuery.childId));
+    const [rows] = await db
+      .select({ count: count() })
+      .from(parentTable)
+      .where(eq(parentTable.id, id))
+      .execute();
+    if (rows.count > 0) {
+      if (deleteChildrens) {
+        await db
+          .delete(childTable)
+          .where(
+            inArray(
+              childTable.id,
+              sql`${db
+                .select({ childId: childParentTable.childId })
+                .from(childParentTable)
+                .where(eq(childParentTable.parentId, id))}`
+            )
+          );
+      }
+      await db.delete(parentTable).where(eq(parentTable.id, id));
       return createSuccessResult(null);
     }
     return createErrorResult(HttpStatusCodes.NOT_FOUND);
