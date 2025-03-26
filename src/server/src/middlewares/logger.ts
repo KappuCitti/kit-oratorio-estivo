3add9401cc518177e1fad3658d75641a9e8c75e8
Benjo332 <manuelparma2005@gmail.com>
2025-03-26 14:55:58 +0100

    SERVER - Fixed logger not showing anything if log didn't come from a web request

--- src/server/src/middlewares/logger.ts
+++ src/server/src/middlewares/logger.ts
@@ -2,7 +2,7 @@ import { createLogger } from '@/utils/logger';
 import { pinoLogger } from 'hono-pino';
 
 export function logger() {
-  const p = createLogger('SERVER', "{req.method} '{req.url}' -> {res.status}");
+  const p = createLogger('SERVER', "{if req.url}{req.method} '{req.url}' -> {res.status} {end}{msg}"); 
 
   return pinoLogger({
     pino: p,
