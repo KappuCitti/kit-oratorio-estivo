5b6c07423e67e5161ac4eff7b1b9fbe6c419f32c
Benjo332 <manuelparma2005@gmail.com>
2025-05-16 10:05:49 +0200

    DATABASE - Added missing autoincrement in activity table

--- src/server/src/database/schema/activities.ts
+++ src/server/src/database/schema/activities.ts
@@ -1,7 +1,7 @@
 import { int, mysqlTable, varchar } from 'drizzle-orm/mysql-core';
 
 export const activityTable = mysqlTable('activities', {
-  id: int().primaryKey(),
+  id: int().primaryKey().autoincrement().notNull(),
   name: varchar({ length: 255 }).notNull(),
   place: varchar({ length: 255 }),
 });
