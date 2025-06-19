ce9a956697b2c1c2352431fdc32f54bbc8609cf4
Benjo332 <manuelparma2005@gmail.com>
2025-06-19 21:05:55 +0200

    SERVER - Added list of classes when getting schools

--- src/server/src/database/school/getSchools.ts
+++ src/server/src/database/school/getSchools.ts
@@ -1,10 +1,23 @@
 import { db } from '..';
-import { like } from 'drizzle-orm';
+import { eq, like } from 'drizzle-orm';
 import { schoolTable } from '../schema/school';
+import { classTable } from '../schema/class';
+import { jsonObjectArray } from '../utils/jsonArray';
 
 export async function getSchools(query: string = '') {
-  const schools = await db.query.schools.findMany({
-    where: like(schoolTable.name, `%${query}%`),
-  });
+  const schools = await db
+    .select({
+      id: schoolTable.id,
+      name: schoolTable.name,
+      canChooseActivities: schoolTable.canChooseActivities,
+      classes: jsonObjectArray({
+        id: classTable.id,
+        name: classTable.name,
+      }),
+    })
+    .from(schoolTable)
+    .innerJoin(classTable, eq(schoolTable.id, classTable.schoolId))
+    .groupBy(schoolTable.id, schoolTable.name, schoolTable.canChooseActivities)
+    .having(like(schoolTable.name, `%${query}%`));
   return schools;
 }
--- src/server/src/models/school.model.ts
+++ src/server/src/models/school.model.ts
@@ -1,6 +1,9 @@
+import { classTable } from '@/database/schema/class';
 import { schoolTable } from '@/database/schema/school';
 import { createSelectSchema } from 'drizzle-zod';
-import type { z } from 'zod';
+import { z } from 'zod';
 
-export const schoolListSchema = createSelectSchema(schoolTable);
+export const schoolListSchema = createSelectSchema(schoolTable).extend({
+  classes: z.array(createSelectSchema(classTable).omit({ schoolId: true })),
+});
 export type SchoolList = z.infer<typeof schoolListSchema>;
